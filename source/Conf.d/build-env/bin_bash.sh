9f46ee47ed8c7f00e89405c3c2f00b1597f7cbb6
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-04-02 21:11:20 +0000

    fix relinking of /bin/sh in Conf.d/build-env/bin_bash.sh

--- source/Conf.d/build-env/bin_bash.sh
+++ source/Conf.d/build-env/bin_bash.sh
@@ -7,7 +7,7 @@ check_required_files $CPREFIX/bin/bash
 
 # relink /bin/sh -> $bash, keep original /bin/sh as /bin/sh.orig
 if [ "$(basename $(readlink -f /bin/sh))" != "bash" ]; then
-	mv /bin/sh /bin/sh.orig
-	ln -s $bash /bin/sh
+	[ -e /bin/sh ] && mv /bin/sh /bin/sh.orig
+	ln -s $CPREFIX/bin/bash /bin/sh
 fi
 
