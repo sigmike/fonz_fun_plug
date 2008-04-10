90ceee5e8cbfe0f3c76756d79df2296b86d4b18c
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-04-10 10:41:33 +0000

    rsyncd start script

---
+++ source/rsync/start-rsyncd.sh
@@ -0,0 +1,14 @@
+#!/ffp/bin/sh
+
+# PROVIDE: rsyncd
+# REQUIRE: LOGIN
+
+. /ffp/etc/ffp.subr
+
+name="rsyncd"
+command="/ffp/bin/rsync"
+rsync_flags="--daemon --config=/ffp/etc/rsyncd.conf"
+required_files="/ffp/etc/rsyncd.conf"
+
+run_rc_command "$1"
+
