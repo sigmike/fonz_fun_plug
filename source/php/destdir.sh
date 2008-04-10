8399a1021ffa852bb95b1e3ea48c58ea550c4877
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-04-10 10:31:22 +0000

    php with enable-pcntl and enable-sigchild

--- source/php/destdir.sh
+++ source/php/destdir.sh
@@ -1,5 +1,9 @@
 make INSTALL_ROOT=$D install
 
+# install php.ini examples
+mkdir -p $D$CPREFIX/etc/examples
+install -m 0644 -o root -g root $S/php.ini-* $D$CPREFIX/etc/examples
+
 # php sucks
 cd $D
 rm -rf .registry .channels .filemap .lock .depdblock .depdb
