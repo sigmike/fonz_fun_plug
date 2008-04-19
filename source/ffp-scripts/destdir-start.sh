4f8cdcd1f60f5dcad887a978058fcb5fea23c771
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-04-19 13:13:03 +0000

    ffp-scripts: removed pwconv start script, included pwconv in passwd.sh

---
+++ source/ffp-scripts/destdir-start.sh
@@ -0,0 +1,3 @@
+cd $D$CPREFIX/start
+chmod 0755 passwd.sh rcS.sh shells.sh telnetd.sh
+
--- source/ffp-scripts/start-telnetd.sh
+++ source/ffp-scripts/start-telnetd.sh
@@ -8,7 +8,6 @@
 name="telnetd"
 command="/ffp/sbin/telnetd"
 telnetd_flags="-l /ffp/bin/sh"
-#telnetd_flags="-l /ffp/bin/login"
 
 run_rc_command "$1"
 
