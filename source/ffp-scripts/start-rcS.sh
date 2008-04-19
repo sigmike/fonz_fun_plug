480048eb023bb4027c2aae09ee69894c469203ff
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-04-19 11:14:20 +0000

    latest rcS start script

--- source/ffp-scripts/start-rcS.sh
+++ source/ffp-scripts/start-rcS.sh
@@ -11,29 +11,41 @@ stop_cmd=:
 
 rcS_start()
 {
-    # fixup firmware issues
+	# fixup firmware issues
 
-    # /dev/random
-    if [ ! -e /dev/random ]; then
-        mknod /dev/random c 1 8
-    fi
+	# /dev/random
+	if [ ! -e /dev/random ]; then
+		mknod /dev/random c 1 8
+	fi
 
-    # /dev/ptmx
-    if [ ! -e /dev/ptmx ]; then
-        mknod -m 0666 /dev/ptmx c 5 2
-    fi
+	# /dev/ptmx
+	if [ ! -e /dev/ptmx ]; then
+		mknod -m 0666 /dev/ptmx c 5 2
+	fi
 
-    # /dev/pts
-    if [ ! -d /dev/pts ]; then
-        mkdir /dev/pts
-    fi
+	# /dev/pts
+	if [ ! -d /dev/pts ]; then
+		mkdir /dev/pts
+	fi
     
-    # devpts mount
-    mount | grep devpts 1>/dev/null 2>/dev/null \
-        || mount -t devpts devpts /dev/pts
+	# devpts mount
+	mount | grep devpts 1>/dev/null 2>/dev/null \
+		|| mount -t devpts devpts /dev/pts
 
-    # writable /dev/null
-    chmod a+w /dev/null
+	# writable /dev/tty and /dev/null
+	chmod 0666 /dev/tty
+	chmod 0666 /dev/null
+
+    	# add essential groups
+	if ! grep -w utmp /etc/group; then
+		groupadd -g 22 utmp
+	fi
+
+	# create essential files
+	mkdir -p /var/run /var/log
+	touch /var/run/utmp /var/log/btmp /var/log/lastlog /var/log/wtmp
+	chgrp utmp /var/run/utmp
+	chmod 0664 /var/run/utmp /var/log/lastlog
 }
 
 run_rc_command "$1"
