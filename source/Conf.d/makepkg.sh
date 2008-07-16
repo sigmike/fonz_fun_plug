e674ed61a9a385bba3618c4755868c72890a5365
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-07-16 15:46:53 +0000

    busybox, dnsmasq, e2fsprogs

--- source/Conf.d/makepkg.sh
+++ source/Conf.d/makepkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 die()
 {
-    cat >2 <<EOF
+    cat >&2 <<EOF
 $@
 EOF
     exit 1
@@ -10,4 +10,4 @@ EOF
 [ $# -eq 1 ] || die "Usage: $0 <package-file>"
 [ -e "$1" ] && rm -f "$1"
 mkdir -p $(dirname "$1")
-tar cf - . | gzip -c -9 >$1.tgz
+tar cf - --exclude=\*~ . | gzip -c -9 >$1.tgz
