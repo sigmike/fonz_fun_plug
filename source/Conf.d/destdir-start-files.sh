ac83d554dbf7dae7cc4e9f26e6dfd5f739e0e6bb
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-08-03 14:29:15 +0000

    destdir-start-files.sh: dont descent into subdirs when looking for start-*.sh

--- source/Conf.d/destdir-start-files.sh
+++ source/Conf.d/destdir-start-files.sh
@@ -1,6 +1,6 @@
 
 # check for start-*.sh files
-for start_file in $(find $X -name start-\*.sh); do
+for start_file in $(find $X -maxdepth 1 -name start-\*.sh); do
 	mkdir -p $D/ffp/start
 	dest_file=$(basename $start_file | sed 's@start-@@')
 	rm -f $D/ffp/start/$dest_file
