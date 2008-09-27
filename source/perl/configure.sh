9f69d1479f113ee32f8de6eda59bb159b73e6269
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2008-09-27 15:46:10 +0000

    perl: perl-5.10

--- source/perl/configure.sh
+++ source/perl/configure.sh
@@ -2,9 +2,12 @@
 sed -i "/linux/ s@-shared@& ${LDFLAGS}@" Configure
 
 ./configure.gnu \
-        --prefix=$CPREFIX \
-        -Dman1dir=$CPREFIX/share/man/man1 \
-        -Dman3dir=$CPREFIX/share/man/man3 \
-        -Dpager="$CPREFIX/bin/less -isR" \
-        -Dusethreads \
+    --prefix=$CPREFIX \
+    -Dvendorprefix=$CPREFIX \
+    -Dman1dir=$CPREFIX/share/man/man1 \
+    -Dman3dir=$CPREFIX/share/man/man3 \
+    -Dpager="$CPREFIX/bin/less -isR" \
+    -Dusethreads \
+    -Dextras="$(echo $(cat $X/extras))" \
+
 
