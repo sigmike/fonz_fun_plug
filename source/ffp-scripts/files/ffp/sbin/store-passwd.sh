dec61e65681fe08c4e473a726e106cabe46380cd
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2009-05-18 08:54:07 +0000

    ffp-scripts: new store-passwd script

--- source/ffp-scripts/files/ffp/sbin/store-passwd.sh
+++ source/ffp-scripts/files/ffp/sbin/store-passwd.sh
@@ -1,25 +1,27 @@
-#!/bin/sh
+#!/ffp/bin/sh
 
-echo "Copying files to mtd1..."
+PATH=/ffp/sbin:/ffp/bin:$PATH
+
+echo "Mounting flash ..."
 mount -t minix /dev/mtdblock0 /sys/mtd1
-cp -f /etc/passwd /sys/mtd1/.
-cp -f /etc/group /sys/mtd1/.
-cp -f /etc/shadow /sys/mtd1/.
-cp -f /etc/samba/smbpasswd /sys/mtd1/.
-#cp -f /etc/ftp_tbl /sys/mtd1/.
-#cp -f /etc/ftpgroup /sys/mtd1/.
+mount -t minix /dev/mtdblock1 /sys/mtd2
+
+echo "Updating files ..."
+for d in /sys/mtd1 /sys/mtd2 /mnt/HD_a4/.systemfile/AccountFile /mnt/HD_b4/.systemfile/AccountFile; do
+    if [ -d "$d" ]; then
+	for f in /etc/passwd /etc/group /etc/shadow /etc/samba/smbpasswd; do
+	    b=$(basename $f)
+	    if [ -e "$d/$b" ]; then
+		echo "  $d/$b"
+		cp -f $f $d
+	    fi
+	done
+    fi
+done
+
+echo "Unmounting ..."
 sync
 umount /sys/mtd1
-    
-echo "Copying files to mtd2..."
-mount -t minix /dev/mtdblock1 /sys/mtd2
-cp -f /etc/passwd /sys/mtd2/.
-cp -f /etc/group /sys/mtd2/.
-cp -f /etc/shadow /sys/mtd2/.
-cp -f /etc/samba/smbpasswd /sys/mtd2/.
-#cp -f /etc/ftp_tbl /sys/mtd2/.
-#cp -f /etc/ftpgroup /sys/mtd2/.
-sync
 umount /sys/mtd2
 
 echo "Done."
