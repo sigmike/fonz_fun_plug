5f40570d3a6c955a3e97f762858d8efb149693aa
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2010-03-01 14:44:56 +0000

    squeezeboxserver 7.4.1

--- source/squeezeboxserver/make.sh
+++ source/squeezeboxserver/make.sh
@@ -7,7 +7,7 @@ rm -rvf CPAN/arch/*
 P5=$CPREFIX/lib/perl5
 cd CPAN
 #$X/clean-cpan.sh $P5/* | $SHELL
-$X/clean-cpan.sh $P5/vendor_perl/* $P5/vendor_perl/*/* | $SHELL
+$X/clean-cpan.sh $P5/5.* $P5/5.*/*-multi $P5/vendor_perl/* $P5/vendor_perl/*/* | $SHELL
 cd ..
 
 # fix interpreters
@@ -15,22 +15,6 @@ PERL=$CPREFIX/bin/perl
 for f in $(find -name \*.pl); do
     sed -i "/^#!/ s@/usr/bin/perl@$PERL@" $f
 done
-
-# build local perl modules
-mkdir -p CPAN/arch  # so build-perl-modules stops moaning
-# patch script to
-# - make it stop asking
-# - use correct perl interpreter
-# - use a different directory to download and build packages
-sed -i.orig \
-    -e '/STDIN/ s@^@# @' \
-    -e "s@/usr/bin/perl@$PERL@" \
-    -e "/\$downloadPath ||=/ s@'\.'@'$TMPDIR'@" \
-    Bin/build-perl-modules.pl
-
-$PERL ./Bin/build-perl-modules.pl JSON::XS YAML::Syck
-
-#    AutoXS::Header Class::XSAccessor Class::XSAccessor::Array
 
 # remove arch-specific binaries
 rm -rf Bin/MSWin* Bin/i386* Bin/darwin* Bin/powerpc*
