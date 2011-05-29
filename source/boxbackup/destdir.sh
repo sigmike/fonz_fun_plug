8941a11a618cc0a33c513b2be7fb1fa146ca1d51
tp <tp@996e54b9-4093-45bc-aa95-dbbc837032dc>
2011-05-29 10:04:38 +0000

    updated squeezeboxserver (and perl mod Audio::Scan)

--- source/boxbackup/destdir.sh
+++ source/boxbackup/destdir.sh
@@ -1,8 +1,7 @@
 
 sed -i "s,/ffp/man/,/ffp/share/man/,g" parcels/*/install-*
 
-mkdir -p $D/ffp/bin
-make DESTDIR=$D install-backup-client
-make DESTDIR=$D install-backup-server
+make install-backup-client
+make install-backup-server
 
 
